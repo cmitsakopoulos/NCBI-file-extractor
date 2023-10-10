d780ad866c59196467776448307c1dea8236640a
Christos Christoforos Mitsakopoulos <mitsacopoulos@gmail.com>
2023-10-10 19:37:12 +0100

    Update movefiles.py

--- movefiles.py
+++ movefiles.py
@@ -1,9 +1,12 @@
 import shutil
 import os
 
-output = r'C:\Users\mitsa\Desktop\Dissertation\T3SS sequences and database\DATABASE'
+#The following is an example directory
+output = r'C:\Users\x\Desktop\Python\'
 
-for count in range(4, 149):
+for count in range(1, 149):
+    #In order for the program to be able to sort through each consecutive folder, one will have to make use of the "%i" % function; 
+    #Our count variable will update with each iteration, allowing the code to sort
     for filetype in os.listdir(r"C:\Users\mitsa\Desktop\Python\FOLDER%i" % count):
         if filetype.endswith(".fna") and count <= 149:
             filepath = os.path.join(r"C:\Users\mitsa\Desktop\Python\FOLDER%i" % count, filetype)
