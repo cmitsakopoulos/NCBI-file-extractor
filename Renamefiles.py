552daf96c18cb3ef1edc483300e9232d215edfd4
Christos Christoforos Mitsakopoulos <mitsacopoulos@gmail.com>
2023-10-10 19:40:55 +0100

    Update Renamefiles.py

--- Renamefiles.py
+++ Renamefiles.py
@@ -1,5 +1,7 @@
 import os
-
+#Below one would insert the directory at which the massive folder is located.
+#Keep in mind, for reasons beyond my understanding at the moment, the program renames the folders containing our FASTA files, and then moves them to the directory in which the program is stored.
+#Take that into account when you insert the desired directory for the "movefiles.py" program
 directory = r''
 count = 0
 for file in os.listdir(directory):
